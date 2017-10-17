f4a36266f38aff340ca242a48b327c2b81f1232f
zhengxuan <zxfishhack@126.com>
2017-10-17 15:59:29 +0800

    add listen address define

--- port_forward.go
+++ port_forward.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/kataras/iris"
 	"./port_forward"
+	"flag"
 )
 
 func main() {
 	app := iris.New()
 	app.StaticWeb("/assets", "./assets")
+	console := flag.String("l", ":8872", "listen addr")
+	flag.Parse()
 	port_forward.RegisterRouter(app)
-	app.Run(iris.Addr("0.0.0.0:8872"))
+	app.Run(iris.Addr(*console))
 }
